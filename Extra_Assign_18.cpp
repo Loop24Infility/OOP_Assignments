37af7003db34bb468bcbfefb9862f2812a924683
Kshitij Jadhav <infilityloop@gmail.com>
2025-04-22 12:11:57 +0530

    Update Extra_Assign_18.cpp

--- Extra_Assign_18.cpp
+++ Extra_Assign_18.cpp
@@ -18,10 +18,10 @@ public:
 
 int main() {
     Student st1;
-    Student st2("Ansh");
+    Student st2("Kshitij");
 
     st1.printName();
     st2.printName();
-
+    cout << "CODE BY KSHITIJ JADHAV";
     return 0;
 }
